df7d255da6d1ac551aa16fcab9e333009e93c6f4
Sergio Mora <sergio94mora@gmail.com>
2018-03-13 15:47:07 -0400

    Se agrega multiples mejores ratings

--- tarea20180313/Tarea20180313.java
+++ tarea20180313/Tarea20180313.java
@@ -2,13 +2,11 @@ package tarea20180313;
 
 import java.io.*;
 
-public class Tarea20180313
-{
+public class Tarea20180313 {
 
-    public static void main(String[] args) throws IOException
-    {
-//        Float[][] ratings = {{2f, 5f, 6.5f, 0.1f, 6f, 8f, 5f}, {1f, 2f, 3f, 4f, 5f, 6f, 7f}, {10f, 5f, 6f, 7f, 4f, 5f, 5f}};
-//        String[] nombreMatinales = {"chv", "c13", "tvn"};
+    public static void main(String[] args) throws IOException {
+//        Float[][] ratings = {{2f, 5f, 6.5f, 0.1f, 6f, 8f, 5f}, {1f, 2f, 3f, 4f, 5f, 6f, 7f}, {10f, 5f, 6f, 7f, 4f, 5f, 5f}, {10f, 5f, 6f, 7f, 4f, 5f, 5f}};
+//        String[] nombreMatinales = {"chv", "c13", "tvn", "cqc"};
 
         int matinales, dias;
         Float[][] ratings;
@@ -32,17 +30,16 @@ public class Tarea20180313
                 } while (ratings[i][j] < 0.1);
             }
         }
+        
         mejoresMatinales(nombreMatinales, ratings);
         promedioRatingMatinal(nombreMatinales, ratings);
         ordenarMatinales(nombreMatinales, ratings);
 
     }
 
-    public static void mejoresMatinales(String[] nombreMatinales, Float[][] ratings)
-    {
+    public static void mejoresMatinales(String[] nombreMatinales, Float[][] ratings) {
 
         imprimir("El matinal con mayor promedio de rating es: ");
-        int mejorMatinal = -1;
         Float promedioMatinal, mejorPromedio;
         mejorPromedio = -10f;
 
@@ -50,49 +47,52 @@ public class Tarea20180313
             promedioMatinal = promedioDeMatinal(ratings[i]);
             if (promedioMatinal > mejorPromedio) {
                 mejorPromedio = promedioMatinal;
-                mejorMatinal = i;
             }
         }
-        imprimir(nombreMatinales[mejorMatinal]);
+
+        for (int i = 0; i < nombreMatinales.length; i++) {
+            promedioMatinal = promedioDeMatinal(ratings[i]);
+            //imprimir("nombre: " + nombreMatinales[i] + " promedio " + promedioMatinal + "/" + mejorPromedio);
+            if (compararFloat(mejorPromedio, promedioMatinal)) {
+                imprimir(nombreMatinales[i]);
+            }
+        }
 
     }
-    
-    public static void ordenarMatinales(String[] nombreMatinales, Float[][] ratings)
-    {
-        for(int i=0;i< nombreMatinales.length-1;i++)
-        {
-            for(int j=i+1;j< nombreMatinales.length;j++)
-            {
-                if(promedioDeMatinal(ratings[i])>promedioDeMatinal(ratings[j])){
+
+    public static void ordenarMatinales(String[] nombreMatinales, Float[][] ratings) {
+        for (int i = 0; i < nombreMatinales.length - 1; i++) {
+            for (int j = i + 1; j < nombreMatinales.length; j++) {
+                if (promedioDeMatinal(ratings[i]) > promedioDeMatinal(ratings[j])) {
                     //Intercambiamos valores
-                    Float[] rating=ratings[i];
-                    ratings[i]=ratings[j];
-                    ratings[j]=rating; 
-                    
-                    String matinalNombre=nombreMatinales[i];
-                    nombreMatinales[i]=nombreMatinales[j];
-                    nombreMatinales[j]=matinalNombre; 
-                    
+                    Float[] rating = ratings[i];
+                    ratings[i] = ratings[j];
+                    ratings[j] = rating;
+
+                    String matinalNombre = nombreMatinales[i];
+                    nombreMatinales[i] = nombreMatinales[j];
+                    nombreMatinales[j] = matinalNombre;
+
                 }
             }
         }
-        
-        for(int i=0;i< nombreMatinales.length;i++)
-        {
-            imprimir("["+i + "] "+nombreMatinales[i] +" prom " + promedioDeMatinal(ratings[i]) );
+        imprimir("Los matinales ordenados son:");
+        for (int i = 0; i < nombreMatinales.length; i++) {
+            imprimir(i+1 + ". " + nombreMatinales[i] + " prom: " + promedioDeMatinal(ratings[i]));
         }
     }
-    
-    public static Float promedioDeMatinal(Float[]rating){
-        Float promedio=0f;
+
+    public static float promedioDeMatinal(Float[] rating) {
+        Float promedio = 0f;
         for (int i = 0; i < rating.length; i++) {
             promedio += (rating[i] / rating.length);
         }
         return promedio;
-    };
+    }
 
-    public static void promedioRatingMatinal(String[] nombreMatinales, Float[][] ratings) throws IOException
-    {
+    ;
+
+    public static void promedioRatingMatinal(String[] nombreMatinales, Float[][] ratings) throws IOException {
         System.out.println("Ingrese el nombre de un matinal, e indique un rango de dias para deciros su promedio en esos dias: ");
         int diaInicio = 0, diaFin = 0;
         String nombreMatinal = ingresarString("Ingrese el nombre del matinal");
@@ -120,13 +120,11 @@ public class Tarea20180313
         }
     }
 
-    public static void imprimir(String mensaje)
-    {
+    public static void imprimir(String mensaje) {
         System.out.println(mensaje);
     }
 
-    public static String leerTeclado(String mensaje) throws IOException
-    {
+    public static String leerTeclado(String mensaje) throws IOException {
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
         imprimir(mensaje);
         String ingresado = lector.readLine();
@@ -134,18 +132,23 @@ public class Tarea20180313
     }
 
     ;
-    public static int ingresarNumero(String mensaje) throws IOException
-    {
+    public static int ingresarNumero(String mensaje) throws IOException {
         return Integer.parseInt(leerTeclado(mensaje));
     }
 
-    public static String ingresarString(String mensaje) throws IOException
-    {
+    public static String ingresarString(String mensaje) throws IOException {
         return leerTeclado(mensaje);
     }
 
-    public static Float ingresarFloat(String mensaje) throws IOException
-    {
+    public static float ingresarFloat(String mensaje) throws IOException {
         return Float.parseFloat(leerTeclado(mensaje));
+    }
+
+    public static boolean compararFloat(float a, float b) {
+
+        if (a - b < 0.00000001 && a - b > -0.00000001) {
+            return true;
+        }
+        return false;
     }
 }
